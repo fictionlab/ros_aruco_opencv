57f700ac454a409cd125e3f079f262f27ae9bd26
Błażej Sowa <bsowa123@gmail.com>
2024-05-07 15:08:45 +0000

    reformat

--- aruco_opencv/include/aruco_opencv/parameters.hpp
+++ aruco_opencv/include/aruco_opencv/parameters.hpp
@@ -93,62 +93,86 @@ inline void declare_aruco_parameters(NodeT && node)
   auto default_parameters = cv::aruco::DetectorParameters::create();
   #endif
 
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.adaptiveThreshWinSizeMin", default_parameters->adaptiveThreshWinSizeMin, 3, 100);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.adaptiveThreshWinSizeMax", default_parameters->adaptiveThreshWinSizeMax, 3, 100);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.adaptiveThreshWinSizeStep", default_parameters->adaptiveThreshWinSizeStep, 1, 100);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.adaptiveThreshConstant", default_parameters->adaptiveThreshConstant, 0.0, 100.0);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.minMarkerPerimeterRate", default_parameters->minMarkerPerimeterRate, 0.0, 4.0);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.maxMarkerPerimeterRate", default_parameters->maxMarkerPerimeterRate, 0.0, 4.0);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.polygonalApproxAccuracyRate",
     default_parameters->polygonalApproxAccuracyRate, 0.0, 0.3);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.minCornerDistanceRate", default_parameters->minCornerDistanceRate, 0.0, 0.25);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.minDistanceToBorder", default_parameters->minDistanceToBorder, 0, 100);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.minMarkerDistanceRate", default_parameters->minMarkerDistanceRate, 0.0, 0.25);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.markerBorderBits", default_parameters->markerBorderBits, 1, 3);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.perspectiveRemovePixelPerCell",
     default_parameters->perspectiveRemovePixelPerCell, 1, 20);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.perspectiveRemoveIgnoredMarginPerCell",
     default_parameters->perspectiveRemoveIgnoredMarginPerCell, 0.0, 0.5);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.maxErroneousBitsInBorderRate",
     default_parameters->maxErroneousBitsInBorderRate, 0.0, 1.0);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.minOtsuStdDev", default_parameters->minOtsuStdDev, 0.0, 30.0);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.errorCorrectionRate", default_parameters->errorCorrectionRate, 0.0, 1.0);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.cornerRefinementMethod", default_parameters->cornerRefinementMethod, 0, 2);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.cornerRefinementWinSize", default_parameters->cornerRefinementWinSize, 2, 10);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.cornerRefinementMaxIterations",
     default_parameters->cornerRefinementMaxIterations, 1, 100);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.cornerRefinementMinAccuracy",
     default_parameters->cornerRefinementMinAccuracy, 0.01, 1.0);
-  declare_param(node,
+  declare_param(
+    node,
     "aruco.detectInvertedMarker", default_parameters->detectInvertedMarker, true);
 
   #if CV_VERSION_MAJOR > 4 || CV_VERSION_MAJOR == 4 && CV_VERSION_MINOR >= 6
-  declare_param(node,
+  declare_param(
+    node,
     "aruco.useAruco3Detection", default_parameters->useAruco3Detection, true);
-  declare_param_int_range(node,
+  declare_param_int_range(
+    node,
     "aruco.minSideLengthCanonicalImg",
     default_parameters->minSideLengthCanonicalImg, 1, 100);
-  declare_param_double_range(node,
+  declare_param_double_range(
+    node,
     "aruco.minMarkerLengthRatioOriginalImg",
     default_parameters->minMarkerLengthRatioOriginalImg, 0.0, 1.0);
   #endif
