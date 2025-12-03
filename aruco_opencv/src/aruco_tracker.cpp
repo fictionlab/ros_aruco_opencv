56a7e95047dce9af5a0b9888a0b9b83f04246d58
Błażej Sowa <bsowa123@gmail.com>
2025-12-03 18:10:32 +0000

    Update copyright information to reflect the correct ownership and years

--- aruco_opencv/src/aruco_tracker.cpp
+++ aruco_opencv/src/aruco_tracker.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 Kell Ideas sp. z o.o.
+// Copyright 2025 Fictionlab sp. z o.o.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
